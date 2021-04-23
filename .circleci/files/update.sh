48d2c3f91402567dfa4bb6730aac89ac544e9066
rahairston <rahairston2014@verizon.net>
2021-04-23 18:35:46 -0500

    Fixing smoke test and path

--- .circleci/files/update.sh
+++ .circleci/files/update.sh
@@ -3,17 +3,17 @@
 
 # Note: This won't do anything if we are already deployed
 echo "Initializing Deployment and Service"
-minikube kubectl -- apply -f ~/Udacity-Capstone/.circleci/files/service.yml
+minikube kubectl -- apply -f ~/udacity-capstone/.circleci/files/service.yml
 minikube kubectl -- apply -f /tmp/modified_deployment.yml
 
 echo "Rolling update!"
 minikube kubectl -- rollout restart deployments/capstone-server
 
-# Run socat to forward port 80 to port 8080
+# Run socat to forward port 8080 to port 8080
 if $(ps -A | grep -q socat)
 then
-echo "Port 80 is already forwarded to 8080"
+echo "Port 8080 is already forwarded to 8080"
 else
-echo "Forwarding localhost port 80 to the NodePort in Minikube server $(minikube service --url capstone-server)"
-sudo nohup socat TCP-LISTEN:80,fork TCP:$(minikube service --url capstone-server | sed -s "s|http://||g") >> /tmp/socat_logs &
+echo "Forwarding localhost port 8080 to the NodePort in Minikube server $(minikube service --url capstone-server)"
+sudo nohup socat TCP-LISTEN:8080,fork TCP:$(minikube service --url capstone-server | sed -s "s|http://||g") >> /tmp/socat_logs &
 fi
